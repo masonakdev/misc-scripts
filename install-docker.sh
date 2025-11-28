7fff4591a3fd9b7b67eb711b2c317edcd5c4183f
Mason <me@masonak.dev>
2025-11-28 09:20:01 +0000

    feat: install docker script

---
+++ install-docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Install Docker and Docker Compose
+
+apt update
+
+apt install ca-certificates curl gnupg lsb-release
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+apt update
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Give user docker permissions
+groupadd docker
+usermod -aG docker $USER
+newgrp docker
